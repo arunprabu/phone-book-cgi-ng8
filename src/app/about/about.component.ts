084e30073dc933dbdea80b4cfa1840de6fe0ca1f
Arun Vijayarengan <arunprabu.v@hotmail.com>
2019-11-27 18:26:16 +0530

    day 3

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  isLoggedIn = true;
+
+  skillsList: string[] = [
+    'html', 'css', 'js'
+  ];
+  
   constructor() { }
 
   ngOnInit() {
